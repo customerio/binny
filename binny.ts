9f2ea9a9ac53b79a251522b827764a28ca10b794
Levi Bostian <levi@customer.io>
2023-09-19 11:03:42 -0500

    fix: runtime error when running command from parsed config properties

--- binny.ts
+++ binny.ts
@@ -11,67 +11,47 @@
  * If you want to update the version of a tool to a newer version, simply update the version number in the toolsUsedInProject array below and push a commit to the repo. Then, all developers and CI servers will automatically download the new version the next time they execute this script.
  * 
  * This script is written in TypeScript and uses Deno as the runtime. In order to execute this script, you need to have Deno installed (brew install deno), or we can compile this script into a binary and run it that way.
- * Command to run this script: deno run --allow-net --allow-read --allow-write --allow-run binny.ts swiftlint --strict 
- * Command to compile this script into a binary: make compile_run_tool
  */
 
-interface Tool {
-  name: string;
-  version: string;
-  downloadUrl: string;
-  pathToBinaryInsideZip: string;
-
-  [propName: string]: string // added to allow the interface to work with stringFormat(). Fixes error, "Index signature for type 'string' is missing in type 'Tool'"
-}
-
 import {existsSync as doesFileExist } from "https://deno.land/std@0.201.0/fs/mod.ts";
-import { format as stringFormat } from "https://deno.land/x/format@1.0.1/mod.ts";
 import { decompress as unzip } from "https://deno.land/x/zip@v1.2.5/mod.ts";
 import {parse as parseYaml} from "https://deno.land/std@0.201.0/yaml/mod.ts";
+import { ToolConfig, Tool } from "./types/tool.ts";
 
 const nameOfToolToRun = Deno.args[0] // get the name of the tool to run from the command line arguments. Example value: 'swiftlint'
-const toolsUsedInProject: Tool[] = parseYaml(Deno.readTextFileSync("binny-tools.yml")) as Tool[]; // Read binny-tools.yml file which is used to define what tools are used in this project.
-const tool: Tool = toolsUsedInProject.find(tool => tool.name === nameOfToolToRun)!
+const toolsUsedInProject: ToolConfig[] = parseYaml(Deno.readTextFileSync("binny-tools.yml")) as ToolConfig[]; // Read binny-tools.yml file which is used to define what tools are used in this project.
+const tool: Tool = new Tool(toolsUsedInProject.find(tool => tool.name === nameOfToolToRun)!)
 
-const rootPathToTool = await assertToolInstalled()
-await runCommand(rootPathToTool)
+await assertToolInstalled()
+await runCommand()
 
-async function assertToolInstalled(): Promise<string> {
-  Deno.mkdirSync("./tools", { recursive: true }); // make the tools directory in case it does not exist. This is where all the tools will be installed
-
-  // This is the file path where we expect the tool to be installed. Each tool has it's own unique directory within the tools/ directory. It's also important to version the directory so that 
-  // the tool can download newer versions if needed. 
-  const toolInstallLocation = `./tools/${tool.name}_${tool.version}`; 
+async function assertToolInstalled(): Promise<void> {
+  Deno.mkdirSync(tool.relativeInstallLocationPath, { recursive: true }); // make the tools directory in case it does not exist. This is where all the tools will be installed
 
   // Check if we have already installed the tool. 
-  if (doesFileExist(toolInstallLocation, { isDirectory: true })) {
-    // Tool exists.
-    return toolInstallLocation
+  if (doesFileExist(tool.relativeInstallLocationPath, { isDirectory: true })) {
+    // Tool exists, quit early.
+    return
   }
 
   // Tool does not exist so it must be installed. 
   // Each tool gets downloaded from GitHub as a zip file. We need to download the zip file and then unzip the file into a specific directory. Once it is unzipped, we consider the tool installed. 
   console.log(`${tool.name} is not yet installed or is out-of-date. Installing ${tool.name}, version: ${tool.version}...`)
-  
-  const downloadZipUrl = stringFormat(tool.downloadUrl, tool); // the download URL is dynamic. Use string format to inject variables into the download URL string to get a versioned URL for the zip file. 
+    
   const downloadedZipFilePath = `/tmp/${tool.name}_${tool.version}.zip`; // We save the tool into the /tmp directory as we do not need it after we unzip the file.   
   
   // download zip file into the /tmp directory
-  const response = await fetch(downloadZipUrl); 
+  const response = await fetch(tool.downloadZipUrl); 
   Deno.writeFileSync(downloadedZipFilePath, new Uint8Array(await response.arrayBuffer()));
 
   // unzip the file into the tools directory. 
-  await unzip(downloadedZipFilePath, toolInstallLocation);
+  await unzip(downloadedZipFilePath, tool.relativeInstallLocationPath);
 
   console.log(`Downloaded ${tool.name}, version ${tool.version}`);
-
-  return toolInstallLocation
 }
 
-// rootPathToTool - example: ./tools/swiftlint_0.39.2 where the tool will be located inside of this given directory. 
-async function runCommand(rootPathToTool: string) {
-  const command = `${rootPathToTool}/${tool.commandToRun}` // example result: ./tools/sourcery_0.39.2/bin/sourcery which is the full path to the binary to execute 
+async function runCommand() {
   const argsToSendToCommand = Deno.args.slice(1); // remove the first argument as it is the name of the tool to run. But we allow passing other args to the command. 
 
-  await new Deno.Command(command, { args: argsToSendToCommand, stdout: "inherit", stderr: "inherit" }).output();
+  await new Deno.Command(tool.commandToExecuteBinary, { args: argsToSendToCommand, stdout: "inherit", stderr: "inherit" }).output();
 }
