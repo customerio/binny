ba7c4c5d8ac6d91a95c79eb05a252b75e4ed78c1
Levi Bostian <levi@customer.io>
2023-09-19 12:20:34 -0500

    get tests passing

--- src/e2e_test.ts
+++ src/e2e_test.ts
@@ -2,7 +2,12 @@ import { assertEquals } from "https://deno.land/std@0.201.0/assert/assert_equals
 import { runTool } from "./runner.ts";
 
 Deno.test("run binny with a real tool, expect it runs command", async () => {
-  Deno.removeSync("./tools", { recursive: true }) // remove the tools directory so we can test that binny installs the tool.
+  try {
+    // if directory does not exist, Deno.removeSync throws an error. Use try/catch and ignore error to prevent this. 
+    Deno.removeSync("./tools", { recursive: true,  }) // remove the tools directory so we can test that binny installs the tool.
+  } catch {
+    // ignore error, tools directory already removed
+  }
   
   const expectedStdout = "2.0.3\n"
 
