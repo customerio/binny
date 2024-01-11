59d14889693ca6da011063a9680023ddd9c4f37a
Levi Bostian <levi@customer.io>
2024-01-11 08:38:21 -0600

    fix: if command binny runs fails, binny should also fail (#10)

--- src/runner.ts
+++ src/runner.ts
@@ -21,10 +21,15 @@ export async function runTool(nameOfToolToRun: string, commandArgsForToolToRun: 
 
 async function runCommand(tool: Tool, commandArgsForToolToRun: string[]): Promise<string> {
   // piping the stdout to allow us to capture it into a variable that we then return from the function. 
-  const output = await new Deno.Command(tool.commandToExecuteBinary, { args: commandArgsForToolToRun, stdout: "piped", stderr: "inherit" }).output();
+  const { code, stdout } = await new Deno.Command(tool.commandToExecuteBinary, { args: commandArgsForToolToRun, stdout: "piped", stderr: "inherit" }).output();
 
-  const outputString = new TextDecoder().decode(output.stdout); // convert the output from a Uint8Array to a string.
+  const outputString = new TextDecoder().decode(stdout); // convert the output from a Uint8Array to a string.
   console.log(outputString) // print the output to the console so the user can see it.
+
+  if (code !== 0) { // if a bash command exits with a non-zero exit code, it means that it failed to run successfully.
+    console.error(`Running command: ${tool.commandToExecuteBinary} ${commandArgsForToolToRun}, failed executing with exit code ${code}.`)
+    Deno.exit(1) // binny should fail if the command that it runs also fails to match the behavior that the user expects.
+  }
 
   return outputString
 }
